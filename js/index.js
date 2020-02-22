eab416a21c683120edc4c95f67de3fda60727e21
TriP22 <60031832+TriP22@users.noreply.github.com>
2020-02-22 23:19:58 +0530

    working

--- js/index.js
+++ js/index.js
@@ -1,20 +1,66 @@
 $(document).ready(function(){
     
-    $('.th-responsive-1').slick({
+    $('.th-slick-responsive-1').slick({
         dots: false,
         infinite: false,
         speed: 300,
         arrows:true,
         slidesToShow: 4,
         slidesToScroll: 4,
+        prevArrow:'<button type="button" class="slick-prev"><i class="las la-angle-left"></i></button>',
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+            }
+          },
+          {
+            breakpoint: 1000,
+            settings: {
+              slidesToShow: 2.5,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 775,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 550,
+            settings: {
+              slidesToShow: 1.5,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 400,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+    });
+
+
+    $('.th-slick-responsive-2').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        arrows:true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         responsive: [
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
+              slidesToShow: 2,
+              slidesToScroll: 2
             }
           },
           {
